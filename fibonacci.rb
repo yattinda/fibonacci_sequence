951294a4531f3c9ec32345409521e88d87b8732a
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-03-16 23:11:36 +0900

    Add answer

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,6 +1,10 @@
 class FibonacciSequence
   def self.fibonacci_item(n)
-    0
+    if n === 0 or n === 1 then
+      return n
+    else
+      return fibonacci_item(n - 2) + fibonacci_item(n - 1)
+    end
   end
 end
 
