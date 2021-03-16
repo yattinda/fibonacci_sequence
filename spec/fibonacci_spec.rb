dbef3f83b289a70e5ae30a5354277be3b65a8b6f
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-03-16 22:01:40 +0900

    Create test case

---
+++ spec/fibonacci_spec.rb
@@ -0,0 +1,16 @@
+require File.expand_path(File.dirname(__FILE__) + '/../fibonacci')
+
+describe BonusDrink do
+  specify { expect(FibonacciSequence.fibonacci_item(0)).to eq 0 }
+  specify { expect(FibonacciSequence.fibonacci_item(1)).to eq 1 }
+  specify { expect(FibonacciSequence.fibonacci_item(2)).to eq 1 }
+  specify { expect(FibonacciSequence.fibonacci_item(3)).to eq 2 }
+  specify { expect(FibonacciSequence.fibonacci_item(4)).to eq 3 }
+  specify { expect(FibonacciSequence.fibonacci_item(5)).to eq 5 }
+  specify { expect(FibonacciSequence.fibonacci_item(6)).to eq 8 }
+  specify { expect(FibonacciSequence.fibonacci_item(7)).to eq 13 }
+  specify { expect(FibonacciSequence.fibonacci_item(8)).to eq 21 }
+  specify { expect(FibonacciSequence.fibonacci_item(9)).to eq 34 }
+  specify { expect(FibonacciSequence.fibonacci_item(10)).to eq 55 }
+end 
+
