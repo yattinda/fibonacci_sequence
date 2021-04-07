573f86846c3fa56f1d9fc0e9d8297506fdfe5de8
yattinda <yattinda@gmail.com>
2021-04-07 15:38:11 +0900

    Fix typo

--- fibonacci.rb
+++ fibonacci.rb
@@ -2,7 +2,6 @@ class FibonacciSequence
   def self.fibonacci_item(n)
     return   if n < 0
     return n if n < 2
-    fibonacci1(n - 1) + fibonacci1(n - 2)
-    end
+    fibonacci_item(n - 1) + fibonacci_item(n - 2)
   end
 end
