bca410a3731d0cb00c081e9a6fb3d35c2e54a243
yattinda <yattinda@gmail.com>
2021-04-07 15:25:05 +0900

    To push

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,6 +1,6 @@
 class FibonacciSequence
   def self.fibonacci_item(n)
-    def fibonacci1(n)
+    def fibonacci1(n) 
     return   if n < 0
     return n if n < 2
     fibonacci1(n - 1) + fibonacci1(n - 2)
