7611ba7e43cca1171df6a02253a25b76e4f31403
yattinda <yattinda@gmail.com>
2021-04-07 15:23:22 +0900

    Nomal def

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,6 +1,9 @@
 class FibonacciSequence
   def self.fibonacci_item(n)
-    0
+    def fibonacci1(n)
+    return   if n < 0
+    return n if n < 2
+    fibonacci1(n - 1) + fibonacci1(n - 2)
+    end
   end
 end
-
