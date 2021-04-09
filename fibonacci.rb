a0f802c455855b37361a0e3cd08a2d736f242e14
yattinda <yattinda@gmail.com>
2021-04-09 12:16:30 +0900

    Test loop

--- fibonacci.rb
+++ fibonacci.rb
@@ -2,6 +2,8 @@ class FibonacciSequence
   def self.fibonacci_item(n)
     return   if n < 0
     return n if n < 2
-    fibonacci_item(n - 1) + fibonacci_item(n - 2)
+    loop do
+      fibonacci_item(n - 1) + fibonacci_item(n - 2)
+    end
   end
 end
