20f44783421b2a60cf66518540db71cce5a4d796
yattinda <yattinda@gmail.com>
2021-04-09 12:25:51 +0900

    Add timeout

--- fibonacci.rb
+++ fibonacci.rb
@@ -2,7 +2,7 @@ class FibonacciSequence
   def self.fibonacci_item(n)
     return   if n < 0
     return n if n < 2
-    loop do
+    loop do  
       fibonacci_item(n - 1) + fibonacci_item(n - 2)
     end
   end
